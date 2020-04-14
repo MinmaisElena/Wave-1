44baa820f47451089cf5f88d3a5a4b282ba056ec
MinmaisElena <55242767+MinmaisElena@users.noreply.github.com>
2020-04-14 14:38:09 -0400

    cylinder

---
+++ vol_of_a_cy.py
@@ -0,0 +1,4 @@
+radius = (float(input("Enter the radius : ")))
+height = float(input("Enter the height : "))
+total = round(radius*radius*3.14*height,1)
+print("The volume of the cylinder is " + str(total))
